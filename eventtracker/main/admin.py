855d157aa2f3ca78c7935d56e4b67baeafe06b7a
marek <marek@marek-laptop.(none)>
2012-05-11 18:32:11 +0200

    style

--- eventtracker/main/admin.py
+++ eventtracker/main/admin.py
@@ -1,7 +1,7 @@
-from main.models import Person, Place, Title, Event
+from main.models import Person, Place, Event, Style
 from django.contrib import admin
 
 admin.site.register(Person)
 admin.site.register(Place)
-admin.site.register(Title)
 admin.site.register(Event)
+admin.site.register(Style)
--- eventtracker/main/models.py
+++ eventtracker/main/models.py
@@ -7,10 +7,14 @@ class Person(models.Model):
     surname = models.CharField(max_length=30)
 
 class Place(models.Model):
+    url = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
 
-class Title(models.Model):
-    name = models.CharField(max_length=50)
+class Style(models.Model):
+    style = models.CharField(max_length=30)
 
 class Event(models.Model):
-    name = models.CharField(max_length=50)
+    place = models.ForeignKey(Place)
+    title = models.CharField(max_length=50)
+    start_date = models.DateField()
+    duration = models.IntegerField()
