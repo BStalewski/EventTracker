76775af5493f6e361ac1dff262cdf64abf82bdc2
marek <marek@marek-laptop.(none)>
2012-05-11 17:26:30 +0200

    renny

--- eventtracker/main/analyzer.py
+++ eventtracker/main/analyzer.py
@@ -11,41 +11,45 @@ import re
 import gzip
 import sys
 
-gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/index.php?module=movie&action=repertoire")
-buf = StringIO( gzip_buf.read())
-html = gzip.GzipFile(fileobj=buf)
-soup = bs.BeautifulSoup(html.read())
-for link in soup.findAll('a'):
-    try:
-        gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/"+link.get('href')) #fixme - zafixowane na stałe cinem-city.pl
-    except:     #fixme - nie czekać na timeout - zrobic lepiej
-        continue
-    buf = StringIO( gzip_buf.read())
-    html = gzip.GzipFile(fileobj=buf)
-    soup2 = bs.BeautifulSoup(html.read())
-    for link2 in soup2.findAll('a'):
-        try:
-            #print "http://www.cinema-city.pl/"+link.get('href')) #fixme - zafixowane na stałe cinem-city.pl
-            print ( "http://www.cinema-city.pl/"+link2.get('href') )
-            gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/"+link2.get('href')) #fixme - zafixowane na stałe cinem-city.pl
-        except:     #fixme - nie czekać na timeout - zrobic lepiej
-            print "except -> ", ( "http://www.cinema-city.pl/"+link2.get('href') )
-            continue
-
-
-        buf = StringIO( gzip_buf.read())
-        html = gzip.GzipFile(fileobj=buf)
-        soup3 = bs.BeautifulSoup(html.read())
-        for name in Person
-
-	print soup3.findAll(text=re.compile('Renny'))[0].findParent().findParent()
-	#for sibling in soup.find(id="Renny").previous_siblings:
-	#	print repr(sibling)
-        #print soup3
-        if 'Renny' in soup3.contents.__str__():
-            print soup3
-        
-    break   #fixme - teraz zatrzymuje się na pierwszym znalezionym żeby było szybciej
-
+#def scan_whole_internet():
 def analyze():
-    return "Something important"
+	gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/index.php?module=movie&action=repertoire")
+	buf = StringIO( gzip_buf.read())
+	html = gzip.GzipFile(fileobj=buf)
+	soup = bs.BeautifulSoup(html.read())
+	for link in soup.findAll('a'):
+		try:
+			gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/"+link.get('href')) #fixme - zafixowane na stałe cinem-city.pl
+		except:     #fixme - nie czekać na timeout - zrobic lepiej
+			continue
+		buf = StringIO( gzip_buf.read())
+		html = gzip.GzipFile(fileobj=buf)
+		soup2 = bs.BeautifulSoup(html.read())
+		for link2 in soup2.findAll('a'):
+			try:
+				#print "http://www.cinema-city.pl/"+link.get('href')) #fixme - zafixowane na stałe cinem-city.pl
+				print ( "http://www.cinema-city.pl/"+link2.get('href') )
+				gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/"+link2.get('href')) #fixme - zafixowane na stałe cinem-city.pl
+			except:     #fixme - nie czekać na timeout - zrobic lepiej
+				print "except -> ", ( "http://www.cinema-city.pl/"+link2.get('href') )
+				continue
+
+			buf = StringIO( gzip_buf.read())
+			html = gzip.GzipFile(fileobj=buf)
+			soup3 = bs.BeautifulSoup(html.read())
+			#for name in Person
+
+			try:
+				print soup3.findAll(text=re.compile('Renny'))[0].findParent().findParent()
+			except:
+				continue
+			#for sibling in soup.find(id="Renny").previous_siblings:
+			#	print repr(sibling)
+			#print soup3
+			#if 'Renny' in soup3.contents.__str__():
+			#	print soup3
+	
+		break   #fixme - teraz zatrzymuje się na pierwszym znalezionym żeby było szybciej
+
+#def analyze():
+    #return "Something important"
