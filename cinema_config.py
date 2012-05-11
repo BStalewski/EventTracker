d383ed386ec0a98fac60a5ff117ede4223d8555a
marek <marek@marek-laptop.(none)>
2012-05-11 12:34:15 +0200

    added naive cinema_citi importer and Soup library

---
+++ cinema_config.py
@@ -0,0 +1,37 @@
+#WARSZAWA			ID_cinema	ID_facepalm
+#	Sadyba			1060		1234
+#	Bemowo			1061		5678
+#	Promenada		1068
+#	Janki			1069
+#	Galeria Mokotow		1070
+#	Arkadia			1074
+#KRAKOW
+#	Plaza			1063
+#	Zakopianka		1064
+#	Galeria Kazimierz	1076
+#	Bonarka			1090
+#WROCLAW			1067
+#POZNAN
+#	Plaza			1078
+#	Kinepolis		1081
+#LODZ				1080
+#GDANSK				1073
+#KATOWICE
+#	Punkt44			1065
+#	Silesia			1079
+#TORUN				1077
+#CZESTOCHOWA
+#	Wolnosc			1075
+#	Galeria Jurajska	1089
+#RUDA SLASKA			1062
+#RYBNIK				1082
+#SOSNOWIEC			1083
+#LUBLIN				1084
+#GLIWICE			1085
+#BYDGOSZCZ			1086
+#ZIELONA GORA			1087
+#BIELSKO-BIALA			1088
+#WALBRZYCH			1091
+#BYTOM				1092
+"1060": 1234,
+"1061": 5678
