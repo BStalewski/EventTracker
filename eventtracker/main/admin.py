d1949e6952a47456d18880cf4087b93a8c195f4a
Bartosz Stalewski <bartekstalewski@gmail.com>
2012-05-11 16:06:10 +0200

    Nothing interesting

--- eventtracker/main/admin.py
+++ eventtracker/main/admin.py
@@ -1,4 +1,7 @@
-from main.models import People
+from main.models import Person, Place, Title, Event
 from django.contrib import admin
 
-admin.site.register(People)
+admin.site.register(Person)
+admin.site.register(Place)
+admin.site.register(Title)
+admin.site.register(Event)
--- eventtracker/main/models.py
+++ eventtracker/main/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class People(models.Model):
+class Person(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+
+class Title(models.Model):
+    name = models.CharField(max_length=50)
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
