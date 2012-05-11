85dabb0363d288f1dea78d32111e2b5d4a1e8811
Bartosz Stalewski <bartekstalewski@gmail.com>
2012-05-11 18:42:33 +0200

    Styles

--- eventtracker/main/analyzer.py
+++ eventtracker/main/analyzer.py
@@ -13,42 +13,70 @@ import sys
 
 #def scan_whole_internet():
 def analyze():
-	gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/index.php?module=movie&action=repertoire")
-	buf = StringIO( gzip_buf.read())
-	html = gzip.GzipFile(fileobj=buf)
-	soup = bs.BeautifulSoup(html.read())
-	for link in soup.findAll('a'):
-		try:
-			gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/"+link.get('href')) #fixme - zafixowane na stałe cinem-city.pl
-		except:     #fixme - nie czekać na timeout - zrobic lepiej
-			continue
-		buf = StringIO( gzip_buf.read())
-		html = gzip.GzipFile(fileobj=buf)
-		soup2 = bs.BeautifulSoup(html.read())
-		for link2 in soup2.findAll('a'):
-			try:
-				#print "http://www.cinema-city.pl/"+link.get('href')) #fixme - zafixowane na stałe cinem-city.pl
-				print ( "http://www.cinema-city.pl/"+link2.get('href') )
-				gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/"+link2.get('href')) #fixme - zafixowane na stałe cinem-city.pl
-			except:     #fixme - nie czekać na timeout - zrobic lepiej
-				print "except -> ", ( "http://www.cinema-city.pl/"+link2.get('href') )
-				continue
+    gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/index.php?module=movie&action=repertoire")
+    buf = StringIO( gzip_buf.read())
+    html = gzip.GzipFile(fileobj=buf)
+    soup = bs.BeautifulSoup(html.read())
+    styles = []
 
-			buf = StringIO( gzip_buf.read())
-			html = gzip.GzipFile(fileobj=buf)
-			soup3 = bs.BeautifulSoup(html.read())
-			for person in Person.objects.all():
-				try:
-					print soup3.findAll(text=re.compile(person.name+" "+person.surname))[0].findParent().findParent()
-				except:
-					continue
-			#for sibling in soup.find(id="Renny").previous_siblings:
-			#	print repr(sibling)
-			#print soup3
-			#if 'Renny' in soup3.contents.__str__():
-			#	print soup3
-	
-		break   #fixme - teraz zatrzymuje się na pierwszym znalezionym żeby było szybciej
+    for link in soup.findAll('a'):
+        try:
+            gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/"+link.get('href')) #fixme - zafixowane na stałe cinem-city.pl
+        except:     #fixme - nie czekać na timeout - zrobic lepiej
+            continue
+        buf = StringIO( gzip_buf.read())
+        html = gzip.GzipFile(fileobj=buf)
+        soup2 = bs.BeautifulSoup(html.read())
+        for link2 in soup2.findAll('a'):
+            try:
+                #print "http://www.cinema-city.pl/"+link.get('href')) #fixme - zafixowane na stałe cinem-city.pl
+                print ( "http://www.cinema-city.pl/"+link2.get('href') )
+                gzip_buf = urllib2.urlopen("http://www.cinema-city.pl/"+link2.get('href')) #fixme - zafixowane na stałe cinem-city.pl
+            except:     #fixme - nie czekać na timeout - zrobic lepiej
+                print "except -> ", ( "http://www.cinema-city.pl/"+link2.get('href') )
+                continue
+
+            buf = StringIO( gzip_buf.read())
+            html = gzip.GzipFile(fileobj=buf)
+            soup3 = bs.BeautifulSoup(html.read())
+            for person in Person.objects.all():
+                try:
+                    foundElements = soup3.findAll(text=re.compile(person.name+" "+person.surname))
+                    if foundElements != []:
+                        print foundElements
+                        for el in foundElements:
+                            print 'AAAAA'
+                            tmpEl = el.findParent() # nalezy zaczac od rodzica, bo napis nie ma stylu
+                            while not tmpEl.get('style'):
+                                print "1", tmpEl
+                                print '2', tmpEl.get('style')
+                                tmpEl = tmpEl.findParent()
+                            print tmpEl
+                            elStyles = tmpEl.get('style').split(';')
+                            for style in elStyles:
+                                if style not in styles:
+                                    styles.append( style )
+
+                        for style in styles:
+                            newElements = soup3.findAll(style=re.compile(style))
+                            print 'TADAAAM'
+                            print newElements
+                            for newEl in newElements:
+                                words = newEl.split(' ')
+                                name = newEl[0]
+                                surname = newEl[1]
+                                print name, surname
+
+                except:
+                    continue
+            #for sibling in soup.find(id="Renny").previous_siblings:
+            #   print repr(sibling)
+            #print soup3
+            #if 'Renny' in soup3.contents.__str__():
+            #   print soup3
+    
+        print "STYLEZ:", styles
+        break   #fixme - teraz zatrzymuje się na pierwszym znalezionym żeby było szybciej
 
 #def analyze():
     #return "Something important"
