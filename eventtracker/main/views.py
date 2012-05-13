f4442bec4f971f84c63227a6b8be2c5c57ac9b66
Bartosz Stalewski <bartekstalewski@gmail.com>
2012-05-13 04:42:05 +0200

    Class bug fixed, added universal analyzer

--- eventtracker/main/views.py
+++ eventtracker/main/views.py
@@ -3,8 +3,16 @@
 from django.http import HttpResponse
 import analyzer
 import mk_analyzer
+import universal_analyzer as ua
 
-def main( request ):
-    content = mk_analyzer.analyze()
-
+def main( request, url='http://multikino.pl/pl/filmy/' ):
+    #content = mk_analyzer.analyze()
+    content = ua.analyze(url)
     return HttpResponse(content)
+
+def multikino( request ):
+    return main(request, url='http://multikino.pl/pl/filmy/')
+
+def cinemacity( request ):
+    return main(request, url='http://www.cinema-city.pl/index.php?module=movie&action=repertoire')
+
