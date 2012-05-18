53eab6289c273fddb397620301ad886efad1c919
marek <marek@marek-laptop.(none)>
2012-05-18 15:21:04 +0200

    a

--- eventtracker/main/models.py
+++ eventtracker/main/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Url_json(models.Model):
+    url   = models.CharField(max_length=100)
+    json  = models.CharField(max_length=300)
+
+class Obiekty(models.Model):
+    pole1 = models.CharField(max_length=50)
+    pole2 = models.CharField(max_length=50)
+    pole3 = models.CharField(max_length=50)
+    pole4 = models.CharField(max_length=50)
+    pole5 = models.CharField(max_length=50)
+    pole6 = models.CharField(max_length=50)
+    url   = models.CharField(max_length=100)
 
 class Person(models.Model):
     name = models.CharField(max_length=30)
