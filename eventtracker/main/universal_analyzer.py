2c21a419f6b12c433af713a95c1870487b08de9f
marek <marek@marek-laptop.(none)>
2012-05-18 20:56:14 +0200

    a

--- eventtracker/main/universal_analyzer.py
+++ eventtracker/main/universal_analyzer.py
@@ -10,6 +10,7 @@ sys.setdefaultencoding('utf-8')
 from models import Obiekt, Url_json
 from cStringIO import StringIO
 from datetime import date, datetime, timedelta
+from collections import deque
 import BeautifulSoup as bs
 import urllib2
 import re
@@ -25,52 +26,58 @@ def teach(rootUrl, teach_index):
 	opener = urllib2.build_opener()
 	opener.add_headers = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.168 Safari/535.19')]
 	gzipped = checkGzipped(rootUrl, opener)
-	soup = bs.BeautifulSoup(getContent(rootUrl, gzipped, opener))
 	styles = []
 	classes = []
 
 	visited = {}
-	visited[rootUrl] = True
-	for link in soup.findAll('a'):
+	link_queue = deque([(rootUrl, 0)])
+	learn_limit = 0
+	while True:
+		if len(link_queue) == 0:
+			print "### PUSTA KOLEJKA LINKOW ###"
+			return
 		try:
-			highUrl = constructUrl(rootUrl, link.get('href'))
-			if highUrl in visited:
-				continue
+			(highUrl, level) = link_queue.popleft()
 			content = getContent(highUrl, gzipped, opener)
 			visited[highUrl] = True
-		except:  #fixme - nie czekać na timeout - zrobic lepiej
+		except RuntimeError as e:
+			#print e, ':', highUrl
 			continue
-		soup2 = bs.BeautifulSoup(content)
-		for link2 in soup2.findAll('a'):
-			# jeżeli tekst w linku jest pusty to to nie jest odnośnik filmu ----------
-			if len(link2.text) == 0 :
-				continue
+		except:  #fixme - nie czekać na timeout - zrobic lepiej
+			#print "except -> ", highUrl
+			continue
+		
+		soup = bs.BeautifulSoup(content)
+		print 'len', len(soup.findAll('a'))
+		for link in soup.findAll('a'):
 			try:
-				lowUrl = constructUrl(highUrl, link2.get('href'))
-				print lowUrl
-				if lowUrl in visited:
-					continue
-				content = getContent(lowUrl, gzipped, opener)
-				visited[lowUrl] = True
+				lowUrl = constructUrl(highUrl, link.get('href'))
 			except RuntimeError as e:
-				print e, ':', link2.get('href')
-			except:  #fixme - nie czekać na timeout - zrobic lepiej
-				print "except -> ", lowUrl
+				#print e, ':', link.get('href')
 				continue
+			except:
+				#print "except -> ", highUrl
+				continue
+			if lowUrl in visited:
+				continue
+			link_queue.append((lowUrl,level+1))
+			visited[lowUrl] = True
 
-			soup3 = bs.BeautifulSoup(content)			   
-			obiekt = Obiekt.objects.all()[teach_index]
-			if not objectFound(obiekt, soup3):
-				continue
-			
-			paths = findPaths(obiekt, soup3)
+		obiekt = Obiekt.objects.all()[teach_index]
+		if objectFound(obiekt, soup):
+			paths = findPaths(obiekt, soup)
 			jsonPaths = json.dumps(paths)
 			
 			newUrlJson = Url_json(url=rootUrl, json=jsonPaths)
 			newUrlJson.save()
-			print 'Nauczylem sie na urlu:', rootUrl
+			print 'Nauczylem sie na urlu:', highUrl
 			return
-		break
+		learn_limit = learn_limit + 1
+		print learn_limit + 1," # ",level," # ",highUrl
+		if learn_limit > 123 or level == 2:
+			print '### NIE NAUCZYLEM SIE!!! ###:', rootUrl
+			return
+		
 	print 'Nie nauczylem sie na urlu:', rootUrl
 
 def getDomain(url):
@@ -81,6 +88,8 @@ def getDomain(url):
 def constructUrl(baseUrl, ahrefUrl):
 	ahrefDomain = getDomain( ahrefUrl)
 	baseDomain = getDomain( baseUrl )
+	if ahrefUrl.startswith('index.php'):	#fixme - qiuck fix do cinema city
+		return baseDomain + "/"+ahrefUrl
 	if ahrefDomain:
 		if baseDomain != ahrefDomain:
 			raise RuntimeError("External domain")
@@ -132,7 +141,6 @@ def notEmptyFields( obj ):
 def objectFound( obj, soup ):
 	fields = notEmptyFields( obj )
 	for field in fields:
-		print len( soup.findAll(text=field) )
 		if len( soup.findAll(text=field) )== 0:
 			return False
 	
